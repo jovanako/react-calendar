596ccd74de527914f173ba90e3808f4d0d23944e
Jovana Kovacevic <jovana1005@gmail.com>
2022-07-04 12:45:33 +0200

    add css to Day component

--- src/components/Day/Day.js
+++ src/components/Day/Day.js
@@ -1,3 +1,5 @@
+import './Day.css'
+
 function isCurrentDay(day, month, year) {
   const currentDate = new Date()
   return (
@@ -7,13 +9,17 @@ function isCurrentDay(day, month, year) {
   )
 }
 
+function getFirstDayInMonth(year, month) {
+  const firstDay = new Date(year, month).getDay()
+  return firstDay === 0 ? 7 : firstDay
+}
+
 export default function Day(props) {
 
   const { day, month, year } = props
   const id = isCurrentDay(day, month, year) ? 'current-day' : undefined
 
-  let firstWeekDayInMonth = new Date(year, month).getDay()
-  firstWeekDayInMonth = firstWeekDayInMonth === 0 ? 7 : firstWeekDayInMonth
+  const firstWeekDayInMonth = getFirstDayInMonth(year, month)
 
   const style = day === 1 ? { gridColumnStart: firstWeekDayInMonth } : {}
 
