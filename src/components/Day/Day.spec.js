12a73c31239ca9a2d4ba23a110faab7166131b16
Jovana Kovacevic <jovana1005@gmail.com>
2022-07-06 21:55:23 +0200

    ignore act warning

--- src/components/Day/Day.spec.js
+++ src/components/Day/Day.spec.js
@@ -1,3 +1,4 @@
+/* eslint-disable testing-library/no-unnecessary-act */
 import React from "react"
 import { render, unmountComponentAtNode } from "react-dom"
 import { act } from "react-dom/test-utils"
