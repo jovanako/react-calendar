64b062af290a542a3b58809d751ab21023ac0314
Jovana Kovacevic <jovana1005@gmail.com>
2022-07-28 22:17:18 +0200

    add onchange property

--- src/components/DayView/DayView.js
+++ src/components/DayView/DayView.js
@@ -1,4 +1,5 @@
 import { useParams } from 'react-router-dom'
+import { useState } from 'react'
 import './DayView.css'
 
 export default function DayView() {
@@ -12,16 +13,22 @@ export default function DayView() {
   const currentHour = String(date.getHours()).padStart(2, '0')
   const currentMinutes = String(date.getMinutes()).padStart(2, '0')
 
+  const [inputValue, setInputValue] = useState(`${year}-${month}-${day}T${currentHour}:${currentMinutes}`)
+
+  function handleInputChange(event) {
+    setInputValue(event.target.value);
+  }
+
   return (
     <div id='day-view'>
       <h1 id='day-view-title'>Add Reminder</h1>
       <form id='reminder-container'>
         <input id='reminder-title' class="reminder-element" type='text' placeholder='Add title'></input>
         <label id='reminder-label' htmlFor='reminder-time'>Date and Time</label>
-        <input type='datetime-local' id='reminder-time' class="reminder-element" value={`${year}-${month}-${day}T${currentHour}:${currentMinutes}`}></input>
+        <input type='datetime-local' id='reminder-time' class="reminder-element" value={inputValue} onChange={handleInputChange}></input>
         <input id='add-button' class="reminder-element" type="submit" value="Add" />
       </form>
-    </div>
+    </div >
   )
 
 }
